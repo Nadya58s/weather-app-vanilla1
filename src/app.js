20d7f2c400715089ffa0c2732a492ed0a21084f0
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-04-24 22:02:49 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 /** @format */
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "3b5f3e51ce51ffad1f79b84acf0944ed";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
