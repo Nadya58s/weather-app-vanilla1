69e77cb08573ce7d1319d1d6bc3f5a77c3934141
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-04-29 13:35:53 +0100

    updated API key

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "3b5f3e51ce51ffad1f79b84acf0944ed";
+  let apiKey = "62bc298785543e137bc6756e514eb1c3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -102,7 +102,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "3b5f3e51ce51ffad1f79b84acf0944ed";
+  let apiKey = "62bc298785543e137bc6756e514eb1c3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
